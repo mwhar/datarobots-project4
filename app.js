651c04eb80bed1a0c5ead5877c979075a0ac41a0
jared.roussel@outlook.com <jared.roussel@outlook.com>
2021-11-22 19:14:00 -0600

    Updated app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+var age = document.getElementById("age").value;
+var minutes = document.getElementById("minutes").value;
+var rebounds = document.getElementById("rebounds").value;
+var assists = document.getElementById("assists").value;
+var steals = document.getElementById("steals").value;
+var blocking = document.getElementById("blocking").value;
+var points = document.getElementById("points").value;
+var games = document.getElementById("games").value;
+var salary = ((561941*age) + (2185*minutes) + (423*rebounds) - (4*assists) + (5137*steals) + (45334*blocking) + (9043*points) - (151833*games))
+
+var button = d3.select("#button")
+
+button.on("click", function(){
+    console.log(salary),
+    d3.select("#salary").text(salary)
+});
+
+//salaryDisplay = ;
